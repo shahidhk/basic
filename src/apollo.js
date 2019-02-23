060052cdbf62edadc33f6b1c2580b175b54f2f29
Shahidh K Muhammed <shahidh@hasura.io>
2019-02-24 02:49:41 +0530

    update api

--- src/apollo.js
+++ src/apollo.js
@@ -6,14 +6,7 @@ import { HttpLink } from 'apollo-link-http';
 
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-export const HASURA_GRAPHQL_ENGINE_HOSTNAME = '192.168.0.103:8080';
-// export const HASURA_GRAPHQL_ENGINE_HOSTNAME = window.location.host;
-
-const scheme = (proto) => {
-  return window.location.protocol === 'https:' ? `${proto}s` : proto;
-}
-
-const httpurl = `${scheme('http')}://${HASURA_GRAPHQL_ENGINE_HOSTNAME}/v1alpha1/graphql`;
+const httpurl = 'https://api.basic.shahidh.in/v1alpha1/graphql';
 
 const getNewClient = (accessKey) => {
 
